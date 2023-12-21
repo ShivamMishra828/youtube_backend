d0414ec0e341761a215f898787bf2fa1ea198a53
ShivamMishra828 <shivam9878522087@gmail.com>
2023-12-21 14:03:16 +0530

    MongoDB atlas connection established and File Structure created

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port: ${PORT}`);
+});
+
+connectDB();
